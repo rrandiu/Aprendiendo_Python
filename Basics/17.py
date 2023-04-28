ff8e5139d873a65607e42160154030795f2fa5e8
rRandiu <limamoricaryt@gmail.com>
2023-04-27 22:16:51 -0500

    Ex 17 Finished

--- Basics/17.py
+++ Basics/17.py
@@ -1,28 +1,66 @@
-discos = {"Latin Disco": 16.30,
-          "Rock": 23.40, "Pop": 27.80,
-          "Salsa": 19.50}
+while True:
+    discos = {"Latin Disco": 16.30, "Rock": 23.40, "Pop": 27.80, "Salsa": 19.50}
 
-cliente = input("Nombre del Cliente: ")
-genero = input("Ingrese el Genero: ")
-cantidad = int(input("Cuantos desea?: "))
-if cantidad <= 3:
-    print(f"Cliente...........: {cliente} \nGenero............: {genero} \nCantidad..........: {cantidad}")
-    print(f"Precio............: S/ {discos.get(genero)}")
-    print(f"Importe  : S/ {round(discos.get(genero) * cantidad, 2)}")
-    print(f"Descuento : S/ 00.0")
-    print(f"Importe  a pagar: {round(discos.get(genero) * cantidad, 2)}")
-    if genero == "Rock" or "Pop":
-        print("Obsequio   : Poster")
-    else:
-        print("Obsequio   : Ninguno")
-elif cantidad > 3:
-    print(f"Cliente...........: {cliente} \nGenero............: {genero} \nCantidad..........: {cantidad}")
-    print(f"Precio............: S/ {discos.get(genero)}")
-    importe = round(discos.get(genero) * cantidad, 2)
-    print(f"Importe  : S/ {importe}")
-    print(f"Descuento : {importe * 0.052}")
-    print(f"Importe  a pagar: {round(discos.get(genero) * cantidad, 2)}")
-    if genero == "Rock" or "Pop":
-        print("Obsequio   : Poster")
-    else:
-        print("Obsequio   : Ninguno")
+    cliente = input("Nombre del Cliente: ")
+    genero = input("Ingrese el Genero: ")
+    cantidad = int(input("Cuantos desea?: "))
+
+    if cantidad <= 3:
+        print("====================================")
+        print(f"Cliente...........: {cliente} \nGenero............: {genero} \nCantidad..........: {cantidad}")
+        print("Precio............: S/ {:.2f}".format(discos.get(genero)))
+        importe = discos.get(genero) * cantidad
+        print("Importe...........: S/ {:.2f}".format(importe))
+        print("Descuento.........: S/ 00.00")
+        print("Importe a pagar...: S/ {:.2f}".format(importe))
+        print("Obsequio..........: Ninguno")
+    if cantidad >= 4:
+        print("====================================")
+        print(f"Cliente...........: {cliente} \nGenero............: {genero} \nCantidad..........: {cantidad}")
+        print("Precio............: S/ {:.2f}".format(discos.get(genero)))
+        importe = discos.get(genero) * cantidad
+        print("Importe...........: S/ {:.2f}".format(importe))
+        descuento = importe * 0.052
+        print("Descuento.........: S/ {:.2f}".format(descuento))
+        subtotal = discos.get(genero) * cantidad
+        print("Importe a pagar...: S/ {:.2f}".format(subtotal - descuento))
+        print("Obsequio..........: Ninguno")
+        print("====================================")
+    if cantidad >= 5:
+        print("====================================")
+        print(f"Cliente...........: {cliente} \nGenero............: {genero} \nCantidad..........: {cantidad}")
+        print("Precio............: S/ {:.2f}".format(discos.get(genero)))
+        importe = discos.get(genero) * cantidad
+        print("Importe...........: S/ {:.2f}".format(importe))
+        descuento = importe * 0.064
+        print("Descuento.........: S/ {:.2f}".format(descuento))
+        subtotal = discos.get(genero) * cantidad
+        print("Importe a pagar...: S/ {:.2f}".format(subtotal - descuento))
+        #
+        #Preguntar sobre el "AND" y "OR"
+        #
+        if genero == "Rock" and cantidad >= 6:
+            print("Obsequio..........: Poster")
+        elif genero == "Pop" and cantidad >= 6:
+            print("Obsequio..........: Poster")
+        else:
+            print("Obsequio..........: Ninguno")
+        print("====================================")
+    if cantidad >= 10:
+        print("====================================")
+        print(f"Cliente...........: {cliente} \nGenero............: {genero} \nCantidad..........: {cantidad}")
+        print(f"Precio............: S/ {repr(discos.get(genero))}")
+        importe = discos.get(genero) * cantidad
+        print(f"Importe...........: S/ {importe}")
+        print(f"Descuento.........: S/ {importe * 0.082}")
+        print(f"Importe a pagar...: {discos.get(genero) * cantidad}")
+        #
+        #Preguntar sobre el "AND" y "OR"
+        #
+        if genero == "Rock" and cantidad >= 6:
+            print("Obsequio..........: Poster")
+        elif genero == "Pop" and cantidad >= 6:
+            print("Obsequio..........: Poster")
+        else:
+            print("Obsequio..........: Ninguno")
+        print("====================================")
